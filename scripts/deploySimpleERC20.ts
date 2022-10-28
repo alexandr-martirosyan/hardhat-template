f55c72f29efe39a3851c9583d962d67a08ae21b2
Alexandr Martirosyan <alexnadr.martirosyan2000@gmail.com>
2022-10-28 16:06:00 +0400

    update script

--- scripts/deploySimpleERC20.ts
+++ scripts/deploySimpleERC20.ts
@@ -3,11 +3,7 @@ import { UpgradeType } from "../helpers/deployments/upgradeType";
 import { SimpleERC20 } from "../typechain-types/contracts/SimpleERC20";
 
 const main = async () => {
-  const token = await deployNewContract<SimpleERC20>(
-    "SimpleERC20",
-    UpgradeType.NON_UPGRADEABLE,
-    []
-  );
+  const token = await deployNewContract<SimpleERC20>("SimpleERC20", UpgradeType.NON_UPGRADEABLE);
 
   console.log("token deployed to the address ->", token.address);
 };
